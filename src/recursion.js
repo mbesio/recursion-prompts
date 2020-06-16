894660edd90d0def38bf469dc574ed65a23284b5
Matteo Besio <matteo.besio@gmail.com>
2020-06-15 23:03:46 -0700

    passed the reverseString function tests

--- src/recursion.js
+++ src/recursion.js
@@ -150,6 +150,11 @@ var powerOfTwo = function(n) {
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+  if(string.length === 1){
+    return string;
+  } else {
+    return string.slice(-1) + reverse(string.slice(0, string.length-1));
+  }
 };
 
 // 10. Write a function that determines if a string is a palindrome.
