6c87f3d9df3c6abfaa14e7706e52ebceeb984c75
Matteo Besio <matteo.besio@gmail.com>
2020-06-15 20:56:08 -0700

    Matteo started working on this

--- src/recursion.js
+++ src/recursion.js
@@ -6,6 +6,8 @@
 // denoted by n!, is the product of all positive integers less than or equal to n.
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
+
+//Matteo starts working on this
 var factorial = function(n) {
 };
 
