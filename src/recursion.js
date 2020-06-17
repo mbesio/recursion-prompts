ccc07aeb471e1b76bde36abdd131f31c038b2545
Matteo Besio <matteo.besio@gmail.com>
2020-06-16 19:55:07 -0700

    passed the paindrome function tests

--- src/recursion.js
+++ src/recursion.js
@@ -159,6 +159,27 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  //convert string to lowercase
+  string = string.toLowerCase();
+  //get rid of any spaces
+  string = string.replace(/\s/g,'');
+
+  if (string.length === 1){
+    return true;
+  }
+
+  if ((string.length === 2 || string.length === 3) && (string[0] === string[string.length -1])){
+    return true;
+  };
+  if(string[0] !== string[string.length -1]){
+    return false;
+  }
+  if(string[0] === string[string.length -1]){
+    return palindrome(string.slice(1, string.length -1));
+  }
+
+
+
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
